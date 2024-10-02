0c703de0c878c634a74e3fbd28e0158ef75a36cb
mullerjay <143011932+mullerjay@users.noreply.github.com>
2024-10-02 09:00:32 -0300

    ..

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a maior montanha do mundo?',
+    'Monte Everest'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Artes',
+    'Quem pintou a obra Mona Lisa?',
+    'Leonardo da Vinci'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Ciencias',
+    'Em que ano o Homem pisou na lua?',
+    '1969'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington'
 )
